b203ce87b1caeb4b4c46a2c4351d33bc1e9d77a5
Alex Silva <alexaugustoti@gmail.com>
2025-10-14 15:43:50 -0300

    fix: SQL migração

--- 06.18/algashop/microservices/billing/src/main/resources/db/migration/V001__create-base-schema.sql
+++ 06.18/algashop/microservices/billing/src/main/resources/db/migration/V001__create-base-schema.sql
@@ -58,7 +58,6 @@ create table public.invoice (
 create index idx_invoice_customer_id on public.invoice (customer_id);
 create index idx_invoice_order_id on public.invoice (order_id);
 create unique index idx_invoice_payment_settings_id on public.invoice (payment_settings_id);
-create unique index idx_invoice_payment_order_id on public.invoice (order_id);
 
 alter table public.invoice add constraint fk_invoice_payment_settings_id foreign key (payment_settings_id) references public.payment_settings(id);
 
@@ -67,7 +66,7 @@ create table public.invoice_line_item (
     items_amount numeric(38,2),
     items_name varchar(255),
     items_number integer,
-    primary key (invoice_id, items_name)
+    primary key (invoice_id, items_number)
 );
 
 create index idx_invoice_line_item_invoice_id on public.invoice_line_item (invoice_id);
